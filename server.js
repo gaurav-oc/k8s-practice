7ee689de56fde219c32c332143f9d9d9b0031bde
Gaurav <gaurav.raheja@oneconvergence.com>
2020-02-20 17:02:17 +0530

    Intial Commit

---
+++ server.js
@@ -0,0 +1,9 @@
+const http=require('http')
+
+var handleRequest = function(request, response) {
+  console.log('Received request for URL: ' + request.url);
+  response.writeHead(200);
+  response.end('Hello World!\n');
+};
+var www = http.createServer(handleRequest);
+www.listen(3000);
